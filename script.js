30b94618664ca5d9c54d225cbcf8a682c9400b94
amenraprod <amenraprod@gmail.com>
2018-09-09 23:58:05 -0400

    added break statements

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ var job = 'principle';
 switch (job){
   case 'teacher':
    console.log(firstName + 'teaches kids how to code.');
+   break;
    case 'driver':
    console.log(firstName + 'drives kids in Uber');
+   break;
    case 'designer':
    console.log(firstName + 'creates PSD designs');
+   break;
    
 }
