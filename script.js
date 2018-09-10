f17c2092d01b06e6f870907589b51792fb4187a4
amenraprod <amenraprod@gmail.com>
2018-09-09 20:56:46 -0400

    new age variables added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-//Operations
+//Operations: Math
 
 var year = 1995;
+var ageJohn = 77;
+var ageSmith = 33;
 var newyear = 2018;
-var year = year - 23;
+var year = ageJohn - 23;
 console.log(year);
 console.log(year * 2);
 console.log(year / 3);
+
+// Logical operators
