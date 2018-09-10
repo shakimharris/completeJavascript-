3fcb93d55f17af348b33c4539672f327c2953791
amenraprod <amenraprod@gmail.com>
2018-09-09 22:49:09 -0400

    multipled x

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ var fullAge = 18;
 var isfullAge = now - yearJohn > fullAge;
 console.log(isfullAge);
 
+//grouping
 var ageJohn = now - yearJohn;
 var ageMark = 35;
 var average = (ageJohn + ageMark) / 2;
 console.log(average);
+
+//multiple values to variables
+var x, y;
+x = (3+5) * 4 -6 //8 *4 = 32. 32 -6 = 26
+console.log(x);
