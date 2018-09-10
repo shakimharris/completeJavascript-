186add5508ed9dd04912d6dacfd2f005494394ed
amenraprod <amenraprod@gmail.com>
2018-09-09 23:34:44 -0400

    added cond logic

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ if (age <13) {
   console.log(firstName + '' + 'is a Boy');
 }
 
-else if {
-  
-}
-else {
+else if () { //between 13 and 20; age >= 13l age < 20;
+  console.log(firstName + 'is a teenager');
+
+} else {
   console.log(firstName + ' ' + 'is a Man');
 }
