3e715918ac40b2e68ea9a69e3dd54532d2c8163a
amenraprod <amenraprod@gmail.com>
2018-09-09 22:38:29 -0400

    removed old js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-//Operations: Math
-
-var year = 1995;
-var ageJohn = 77;
-var ageSmith = 33;
-var newyear = 2018;
-var year = ageJohn - 23;
-console.log(year);
-console.log(year * 2);
-console.log(year / 3);
-
-// Logical operators
-var johnOlder = ageJohn > ageSmith;
-console.log(ageJohn > ageSmith);
-//type of operators
-
-console.log(typeof johnOlder);
+//Operation precedence
