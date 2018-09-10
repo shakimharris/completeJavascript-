1a24e77c7901c6435d6f893b890224616b42b199
amenraprod <amenraprod@gmail.com>
2018-09-09 23:33:02 -0400

    added elseif

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 var firstName = 'Shakim';
 var age = 18;
 
-if(age <18){
+if (age <13) {
   console.log(firstName + '' + 'is a Boy');
+}
+
+else if {
+  
+}
+else {
+  console.log(firstName + ' ' + 'is a Man');
 }
