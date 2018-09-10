af555fe0ff5174cce55bc8372ec1139be3574566
amenraprod <amenraprod@gmail.com>
2018-09-09 23:56:16 -0400

    added switch statements

---
+++ script.js
@@ -0,0 +1,11 @@
+//switch statement
+var job = 'principle';
+switch (job){
+  case 'teacher':
+   console.log(firstName + 'teaches kids how to code.');
+   case 'driver':
+   console.log(firstName + 'drives kids in Uber');
+   case 'designer':
+   console.log(firstName + 'creates PSD designs');
+   
+}
