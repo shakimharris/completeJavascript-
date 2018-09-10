a5469d4336ea8231fa685ec82c6af29b5a0e816c
amenraprod <amenraprod@gmail.com>
2018-09-10 01:24:11 -0400

    completed switch statement

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var whatdoyouDo = function (name, birthYear){
     case 'teacher':
     return firstName + 'teaches how to code';
     case 'driver':
-
+    return firstName + 'drives a Lyft';
     case 'designer':
+    return firstName + 'creates web designs';
 
     default:
+    return firstName + 'does other things';
   }
 }
