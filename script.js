277054573e28adaa5aad92f0e3b95cb5d830d14e
amenraprod <amenraprod@gmail.com>
2018-09-09 23:29:46 -0400

    removed prev logic

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-///If else statements
-var firstName = 'Shakim';
-var civilStatus = 'single';
-
-if (civilStatus === 'married'){
-  console.log(firstName + ' ' + 'is' + 'Married!');
-}
-else {
-  console.log(firstName + ' ' + 'will be married soon :)');
-}
-var isMarried = true;
-if (isMarried){
-  console.log(firstName + ' ' + 'is' + 'Married!');
-}
-else {
-  console.log(firstName + ' ' + 'will be married soon :)');
-}
+//Boolean logic 
