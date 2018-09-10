9f1044ab1d15bc3da1593dff1fc9270549141b60
amenraprod <amenraprod@gmail.com>
2018-09-09 23:44:08 -0400

    removed old logic

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-//Boolean logic
-var firstName = 'Shakim';
-var age = 20;
 
-if (age <13) {
-  console.log(firstName + '' + 'is a Boy');
-}
-
-else if (age >= 13 && age < 20) {
-  console.log(firstName + 'is a teenager');
-} else  if (age > 20 && age < 30){
-  console.log(firstName + 'is a young man');
-}
-else {
-  console.log(firstName + ' ' + 'is a Man');
-}
