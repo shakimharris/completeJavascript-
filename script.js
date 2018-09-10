a809f87892bf9a764b7c1d596e5f25eff13a721f
amenraprod <amenraprod@gmail.com>
2018-09-09 23:52:17 -0400

    removed data

--- script.js
+++
@@ -1,9 +0,0 @@
-//Ternary
-
-var firstName = 'Shakim';
-var age = 23;
-
-age >= 18 ? console.log(firstName + 'drinks beer')
-: console.log(firstName + 'drinks juice');
-
-var drink = age >= 18 ? 'beer' : 'juice';
