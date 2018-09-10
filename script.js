8d1a8f25d33d0f1dba059e5890f776dd1ad56917
amenraprod <amenraprod@gmail.com>
2018-09-10 01:32:08 -0400

    arrays

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-//function expressions
-var whatdoyouDo = function (job, firstName){
-  switch(job){
-    case 'teacher':
-    return firstName + 'teaches how to code';
-    case 'driver':
-    return firstName + 'drives a Lyft';
-    case 'designer':
-    return firstName + 'creates web designs';
-
-    default:
-    return firstName + 'does other things';
-  }
-}
-console.log(whatdoyouDo('teacher', 'John'));
-console.log(whatdoyouDo('designer', 'Kristina'));
-console.log(whatdoyouDo('driver', 'Umar'));
+//Arrays 
